e2a1ac4b66dd269524547b8ff38ba31111ccffb2
dw it's just me <email@example.com>
2025-06-16 07:00:04 +0200

    cleaned up a little

--- src/board.py
+++ src/board.py
@@ -24,8 +24,14 @@ class Board(object):
     _map: list[list[CellRef]]
     _weights: dict[tuple[int, int], int]
 
+
+# /////////////////////////////////////////
+# --- Вспомогательные методы --------------
+# /////////////////////////////////////////
+
+
     @staticmethod
-    def rotate45(grid: list[list[T]], reverse: bool = False) -> list[list[T]]:
+    def __rotate45(grid: list[list[T]], reverse: bool = False) -> list[list[T]]:
         n = len(grid)
         result: list[list[T]] = [[] for _ in range(2 * n - 1)]
         for i in range(n):
@@ -37,13 +43,13 @@ class Board(object):
         return result
 
     @staticmethod
-    def rotate90(grid: list[list[T]]) -> list[list[T]]:
+    def __rotate90(grid: list[list[T]]) -> list[list[T]]:
         n = len(grid)
         return [[grid[i][j] for i in range(n)] for j in range(n)]
 
     @staticmethod
-    def find_all_lines(grid: list[list[T]], length: int) -> list[list[T]]:
-        all_rows = [*grid, *Board.rotate45(grid), *Board.rotate90(grid), *Board.rotate45(grid, reverse=True)]
+    def __find_all_lines(grid: list[list[T]], length: int) -> list[list[T]]:
+        all_rows = [*grid, *Board.__rotate45(grid), *Board.__rotate90(grid), *Board.__rotate45(grid, reverse=True)]
         for i in range(len(all_rows) - 1, -1, -1):
             if len(all_rows[i]) < length:
                 all_rows.pop(i)
@@ -56,11 +62,23 @@ class Board(object):
     def __generate_ref_grid(size: int) -> list[list[CellRef]]:
         return [[CellRef(j, i) for i in range(size)] for j in range(size)]
 
+    def __ref2state(self: Self, row: list[CellRef]) -> list[CellState]:
+        return list(map(CellRef.get_lambda(self._grid), row))
+
+    def _get_weight(self: Self, ref: CellRef) -> int:
+        return self._weights[ref.to_tuple()]
+
+
+# /////////////////////////////////////////
+# --- Встроенные методы -------------------
+# /////////////////////////////////////////
+
+
     def __init__(self: Self, size: int) -> None:
         if size not in [3, 4, 5, 6]:
             raise BoardException("Size must be from 3 to 6.")
         self._grid = [[CellState.EMPTY for _ in range(size)] for _ in range(size)]
-        self._map = Board.find_all_lines(Board.__generate_ref_grid(size), WINNING_LENGTH_PER_GRID_SIZE[size])
+        self._map = Board.__find_all_lines(Board.__generate_ref_grid(size), WINNING_LENGTH_PER_GRID_SIZE[size])
         reflist: list[CellRef] = []
         for row in Board.__generate_ref_grid(size):
             reflist += row
@@ -69,26 +87,26 @@ class Board(object):
             count_func: Callable[[list[CellRef]], int] = lambda l: l.count(ref)
             self._weights[ref.to_tuple()] = sum(list(map(count_func, self._map)))
 
-    def get_weight(self: Self, ref: CellRef) -> int:
-        return self._weights[ref.to_tuple()]
-
-    def size(self: Self) -> int:
-        return len(self._grid)
-
     def __str__(self: Self) -> str:
         result: list[str] = []
         for row in self._grid:
             result.append(' | '.join(list(map(str, row))))
         return ('\n' + '--+-' * (self.size() - 1) + '-\n').join(result)
 
+    def size(self: Self) -> int:
+        return len(self._grid)
+
+
+# /////////////////////////////////////////
+# --- Анализ игры  ------------------------
+# /////////////////////////////////////////
+
+
     def is_full(self: Self) -> bool:
         for row in self._grid:
             if CellState.EMPTY in row:
                 return False
         return True
-
-    def __ref2state(self: Self, row: list[CellRef]) -> list[CellState]:
-        return list(map(CellRef.get_lambda(self._grid), row))
 
     def check_for_win(self: Self) -> CellState:
         for row in self._map:
@@ -109,5 +127,4 @@ class Board(object):
                         if ref.get(self._grid) == CellState.EMPTY:
                             result[side].append(ref)
         return result
-
 
